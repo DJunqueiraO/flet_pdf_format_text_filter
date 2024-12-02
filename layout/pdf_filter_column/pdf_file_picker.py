289fe17545353615235484b62bd260f282a752db
DJunqueiraO <danielpotoco@gmail.com>
2024-12-02 19:04:11 -0300

    Organização de codigo

--- layout/pdf_filter_column/pdf_file_picker.py
+++
@@ -1,6 +0,0 @@
-import flet
-
-
-class PdfFilePicker(flet.FilePicker):
-    def __init__(self, **cfg):
-        super().__init__(**cfg)
--- layout/pdf_filter_column/pdf_filter_column.py
+++ layout/pdf_filter_column/pdf_filter_column.py
@@ -3,9 +3,8 @@ import flet
 from layout.loading_container.loading_container import LoadingContainer
 from layout.pdf_filter_column.book_text_column.book_text_column import BookTextColumn
 from layout.pdf_filter_column.filters_row.filters_row import FiltersRow
-from layout.pdf_filter_column.get_text_button import GetTextButton
+from layout.pdf_filter_column.get_text_button.get_text_button import GetTextButton
 from layout.pdf_filter_column.inputs_row.inputs_row import InputsRow
-from layout.pdf_filter_column.pdf_file_picker import PdfFilePicker
 
 
 class PdfFilterColumn(flet.Column):
@@ -17,7 +16,7 @@ class PdfFilterColumn(flet.Column):
     ):
 
         text_filters_row = FiltersRow()
-        pdf_file_picker = PdfFilePicker()
+        pdf_file_picker = flet.FilePicker()
         book_text_column = BookTextColumn()
         get_text_button = GetTextButton(
             file_picker=pdf_file_picker,
